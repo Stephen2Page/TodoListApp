604d61c8c4eaae1fcb15e84fb163a8f74eaa2d33
Stephen2Page <stephen2page@gmail.com>
2016-10-26 15:25:01 -0400

    attempted to disable authentication, not working now

---
+++ TodoListApp/Models/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace TodoListApp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+
+        public ApplicationDbContext() : base("name=ApplicationDbContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<TodoListApp.Models.List> Lists { get; set; }
+
+        public System.Data.Entity.DbSet<TodoListApp.Models.Item> Items { get; set; }
+    }
+}
